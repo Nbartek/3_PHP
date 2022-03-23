ee8df556c4ed1c94a078b7d4aeffdbd486936fa6
Nbartek <64834216+Nbartek@users.noreply.github.com>
2022-03-24 00:46:45 +0100

    hjhj

--- ark3_ambulans/dodanie.php
+++ ark3_ambulans/dodanie.php
@@ -1,2 +1,8 @@
 <?PHP
-//$conn = mysqli("localhost",null,"root","3tipg_ambulans");
+$conn = new mysqli("localhost",'root',null,"3tipg_ambulans");
+$conn = trim($_POST['jeden']);
+$conn = trim($_POST['dwa']);
+$conn = trim($_POST['trzy']);
+$conn = strval($_POST['karetka']);
+$sql = "INSERT INTO `ratownicy`(`nrKaretki`, `ratownik1`, `ratownik2`, `ratownik3`) VALUES ('20','Adam Nowak','Adam Nowak','Anna Nowakowska')";
+
