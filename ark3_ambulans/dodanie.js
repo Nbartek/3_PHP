5ff0e27012bd583f617b2bbb51dc7a293fa73cc0
Nbartek <64834216+Nbartek@users.noreply.github.com>
2022-03-24 14:17:51 +0100

    dfgdf
    dfgdf

---
+++ ark3_ambulans/dodanie.js
@@ -0,0 +1,38 @@
+var jeden = document.querySelector("#jeden");
+var dwa = document.querySelector("#dwa");
+var trzy = document.querySelector("#trzy");
+var karetka = document.querySelector("#karetka");
+
+document.querySelector("#ambulans").onsubmit = function() {
+    console.log(isValidForm());
+    AjaxTable();
+    return isValidForm();
+};
+function isValidForm(){
+    if(karetka.value.trim() == ""){
+        karetka.style.backgroundColor = 'red'; 
+        return false;
+    }else{karetka.style.backgroundColor = 'white'; };
+    if(jeden.value.trim() == ""){
+        jeden.style.backgroundColor = 'red'; 
+        return false;
+    }else{jeden.style.backgroundColor = 'white'; };
+    if(dwa.value.trim() == ""){
+        dwa.style.backgroundColor = 'red'; 
+        return false;
+    }else{dwa.style.backgroundColor = 'white'; };
+    if(trzy.value.trim() == ""){
+        trzy.style.backgroundColor = 'red'; 
+        return false;
+    }else{trzy.style.backgroundColor = 'white'; };
+
+    if(jeden.value.trim() !="" && dwa.value.trim()!=""&&trzy.value.trim()!=""&&karetka.value.trim()!=""){
+        return true;
+    }
+
+}
+function AjaxTable(){
+    var ajax = XMLHttpRequest();
+
+
+}
--- ark3_ambulans/dodanie.php
+++ ark3_ambulans/dodanie.php
@@ -1,8 +1,25 @@
 <?PHP
-$conn = new mysqli("localhost",'root',null,"3tipg_ambulans");
-$conn = trim($_POST['jeden']);
-$conn = trim($_POST['dwa']);
-$conn = trim($_POST['trzy']);
-$conn = strval($_POST['karetka']);
-$sql = "INSERT INTO `ratownicy`(`nrKaretki`, `ratownik1`, `ratownik2`, `ratownik3`) VALUES ('20','Adam Nowak','Adam Nowak','Anna Nowakowska')";
+if (isset($_POST['submit'])) {
+    add();
+}
+function add()
+{
+    $conn = new mysqli("localhost", 'root', null, "3tipg_ambulans");
+    $jeden = trim($_POST['jeden']);
+    $dwa = trim($_POST['dwa']);
+    $trzy = trim($_POST['trzy']);
+    $karetka = strval($_POST['karetka']);
+    $sql = "INSERT INTO `ratownicy`(`nrKaretki`, `ratownik1`, `ratownik2`, `ratownik3`) VALUES ('$karetka','$jeden','$dwa','$trzy')";
+    if (empty($jeden) && empty($dwa) && empty($trzy) && empty($ambulans)) {
+        echo ("<span color:red>Nie wprowadziłeś wszystkiego</span>");
+    } else {
+        $conn->query($sql);
+        echo ("Do bazy zostało wysłane zapytanie: $sql");
+    }
 
+    $conn->close();
+    header("Location: ratownicy.html");
+}
+function display(){
+    
+}
