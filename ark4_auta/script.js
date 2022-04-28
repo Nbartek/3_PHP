c4b1a6c840aa228a5bea98f9020e44da3a983129
Nbartek <64834216+Nbartek@users.noreply.github.com>
2022-04-28 14:19:12 +0200

    dfsdf
    sd

--- ark4_auta/script.js
+++ ark4_auta/script.js
@@ -2,14 +2,53 @@ var color1 = document.querySelector("#Indigo");
 var color2 = document.querySelector("#SteelBlue");
 var color3 = document.querySelector("#Olive");
 var font = document.querySelector("#font");
-console.log(color1.value);
+var fontSizee = document.querySelector("#font-size");
+var border = document.querySelector("#ifborder");
+//var OL = document.querySelector(".lista");
+console.log(document.querySelector('input[type = radio]:checked').checked.value);
+
+var kolory = ['white','blue','green','yellow'];
+
+color1.addEventListener("click", () => { changeBackground(color1.value) });
+color2.addEventListener("click", () => { changeBackground(color2.value) });
+color3.addEventListener("click", () => { changeBackground(color3.value) });
+font.addEventListener('change', changeFont);
+fontSizee.addEventListener('focusout', ()=>{changeFontSize(fontSizee.value)});
+border.addEventListener('change', ()=>{changeBorder(border.checked)});
+document.querySelector('input[type = radio]:checked').addEventListener('change',()=>{changeOL(document.querySelector('input[type = radio]:checked')).value})
+addColors(kolory);
 function changeBackground(color) {
     document.querySelector(".right").style.backgroundColor = color;
 }
 function changeFont() {
     document.querySelector(".right").style.color = font.value;
 }
-color1.addEventListener("click", changeBackground);
-color2.addEventListener("click", changeBackground(color2.value));
-color3.addEventListener("click", changeBackground(color3.value));
-font.addEventListener('change', changeFont)
+function changeFontSize(){
+    document.querySelector(".right").style.fontSize = fontSizee.value+'%';
+    console.log(fontSizee.value+'%');
+    console.log(fontSizee.value);
+}
+function changeBorder(checked){
+    if(checked == true){
+        document.querySelector("#dababy").style.border = "solid 1px white";
+    }else if(checked == false)
+    document.querySelector("#dababy").style.border = "0";
+}
+function changeOL(radioValue){
+    if(radioValue = "disc" ){
+        document.querySelectorAll(".qwe").style.listStyleType = "disc";
+    }else if(radioValue = "square"){
+        document.querySelectorAll(".qwe").style.listStyleType = "square";
+    }else{
+        document.querySelectorAll(".qwe").style.listStyleType = "circle";
+    }
+}
+function addColors(colors){
+    for(color of colors){
+       var z = document.createElement("button");
+       z.id = ''+color+'';
+       z.style.backgroundColor = ''+color+'';
+       z.innerHTML = ''+color+'';
+        document.querySelector("#for4").appendChild(z);
+    }
+}
