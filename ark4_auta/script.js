580b1a4de3b7d238426ae7634da959b479999f9a
Nbartek <64834216+Nbartek@users.noreply.github.com>
2022-04-28 02:40:31 +0200

    efsdgfsd
    d

--- ark4_auta/script.js
+++ ark4_auta/script.js
@@ -1,10 +1,15 @@
 var color1 = document.querySelector("#Indigo");
 var color2 = document.querySelector("#SteelBlue");
 var color3 = document.querySelector("#Olive");
+var font = document.querySelector("#font");
 console.log(color1.value);
 function changeBackground(color) {
     document.querySelector(".right").style.backgroundColor = color;
 }
-color1.addEventListener("click", changeBackground(color1.value));
+function changeFont() {
+    document.querySelector(".right").style.color = font.value;
+}
+color1.addEventListener("click", changeBackground);
 color2.addEventListener("click", changeBackground(color2.value));
 color3.addEventListener("click", changeBackground(color3.value));
+font.addEventListener('change', changeFont)
